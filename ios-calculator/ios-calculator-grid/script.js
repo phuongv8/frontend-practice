12bd62c7cec4cb49ecfbf4b82540710302daa41b
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2023-04-29 22:30:14 -0700

    Handle division by zero

--- ios-calculator/ios-calculator-grid/script.js
+++ ios-calculator/ios-calculator-grid/script.js
@@ -22,13 +22,13 @@ function buttonClick(value) {
 }
 
 function handleNumber(number) {
-  console.log(justCalculated);
   if (buffer === '0' || justCalculated) {
     buffer = number;
     justCalculated = false;
   } else {
     buffer += number;
   }
+  console.log(buffer);
 }
 
 function handleSymbol(symbol) {
@@ -53,22 +53,18 @@ function handleSymbol(symbol) {
       justCalculated = true;
       if (prevOperator === null) {
         return;
-      } else {
-        performOperation(parseInt(buffer));
-        prevOperator = null;
-        buffer = '' + calculateTotal;
-        calculateTotal = 0;
       }
+
+      performOperation(parseInt(buffer));
+      prevOperator = null;
+      buffer = '' + calculateTotal;
+      calculateTotal = 0;
 
       break;
   }
 }
 
 function performOperation(symbol) {
-  if (buffer === '0') {
-    return;
-  }
-
   const intBuffer = parseInt(buffer);
   if (calculateTotal === 0) {
     calculateTotal = intBuffer;
@@ -82,18 +78,16 @@ function performOperation(symbol) {
 
 function applyMath(intBuffer) {
   if (prevOperator in mathOperations) {
-    calculateTotal = mathOperations[prevOperator](calculateTotal, intBuffer);
+    if (prevOperator === '÷' && intBuffer === 0) {
+      calculateTotal = Infinity;
+    } else {
+      calculateTotal = mathOperations[prevOperator](calculateTotal, intBuffer);
+    }
   }
 }
 
 function updateScreen() {
   screen.innerText = buffer;
-}
-
-function resetCalculator() {
-  buffer = '0';
-  calculateTotal = 0;
-  prevOperator = null;
 }
 
 function init() {
