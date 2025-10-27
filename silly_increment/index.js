1b153ef099aafce30f4ecb4fb2073f82a749d74b
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2025-10-26 21:29:48 -0700

    Implement save option

--- silly_increment/index.js
+++ silly_increment/index.js
@@ -1,4 +1,16 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
 function increment() {
-    console.log("Increment button clicked");
+    count += 1
+    countEl.textContent = count
 }
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
