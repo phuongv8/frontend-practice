138bc2a6ed22765a36f7a0b11c0a922e285d47a2
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2025-11-15 21:32:34 -0800

    Add text input to get length

---
+++ password_generator/index.js
@@ -0,0 +1,7 @@
+let generateBtn = document.getElementById("generate-btn");
+let passwordsContainer = document.querySelector(".passwords-container");
+let lengthInput = document.getElementById("length-input");
+function printInput() {
+    console.log(lengthInput.value);
+}
+
